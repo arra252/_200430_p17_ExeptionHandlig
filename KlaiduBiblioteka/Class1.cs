ac9b625151b00af2ffcf3ae4f91cb92d177c8e04
Arunas <arra252@gmail.com>
2020-04-30 21:37:23 +0300

    Add project files.

---
+++ KlaiduBiblioteka/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlaiduBiblioteka
+{
+    public class SavoExeption : Exception
+    {
+        public String KodoDalisIskvieteKlaida { get; set; }
+
+        public override string Message => "mano Klaida";
+        
+    }
+
+    public class SaiciuDarboKlase
+    {
+        public int GrandParent(int kelintas)
+        {
+            return Parent(kelintas);
+        }
+        public int Parent(int kelintas)
+        {
+            return GautiSkaiciu(kelintas);
+        }
+
+        private int GautiSkaiciu(int kelintas)
+        {
+            int[] skaiciai = { 1, 2, 3, 4, 5, 6 };
+            if (kelintas<0)
+            {
+                throw new SavoExeption();
+            }
+            return skaiciai[kelintas];
+        }
+    }
+}
---
+++ _200430_p17_ExeptionHandlig/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KlaiduBiblioteka;
+
+namespace _200430_p17_ExeptionHandlig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SaiciuDarboKlase darboKlase = new SaiciuDarboKlase();
+
+                int skaicius = darboKlase.GrandParent(2);
+                Console.WriteLine(skaicius);
+                int? skai = null;
+                Console.WriteLine(darboKlase.GrandParent(-1));
+
+                //Console.WriteLine(darboKlase.GrandParent(58));
+                Console.WriteLine("Programa baigia darba");
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                //Console.WriteLine(ex.StackTrace);
+                Console.WriteLine("Programa po klaidos pranesimu");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("PerduotasNull");
+            }
+            catch (SavoExeption ex)
+            {
+                Console.WriteLine(ex.KodoDalisIskvieteKlaida);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("nenumatyta klaida" + ex.Message);
+                Console.WriteLine(ex.GetType());
+                Console.WriteLine(ex.StackTrace);
+            }
+
+
+        }
+
+    }
+}
